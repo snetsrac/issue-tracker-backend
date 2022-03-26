ab03080c53979e9bbe4fc7d9fd8cbde34e67fb01
snetsrac <dev@chriscarstens.com>
2022-03-26 20:39:43 +0000

    Add CORS for dev client

---
+++ src/main/java/com/snetsrac/issuetracker/config/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.snetsrac.issuetracker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+    
+}
